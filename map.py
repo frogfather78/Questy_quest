790d0c6316e55d99333882e5b2f08fe38a5540ff
frogfather78 <frogfather@gmail.com>
2016-11-24 07:08:33 +0000

    map
    its not much of a map yet

---
+++ map.py
@@ -0,0 +1,14 @@
+
+class Map():
+	"""around which one can navigate"""
+	
+	def __init__():
+		self.floor_plan = [4,
+			0, 0, 3, 0,
+			2, 2, 2, 0,
+			1, 2, 2, 2]
+		
+		#1 = first room
+		#2 = any other room
+		#3 = last room
+		#first entry = width of array
