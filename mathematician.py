179c78b123e14cd5240e1d315a7122fa08eefa53
Phil <frogfather@gmail.com>
2018-05-11 09:45:12 +0100

    spacing indentation nonsense

--- mathematician.py
+++ mathematician.py
@@ -1,12 +1,16 @@
 from random import randint
 
-#initialise experience level
+
+# initialise experience level
 
 class Player():
-	"""someone who plays the game"""
-	def __init__(self):
-		self.exp = 1
-		#self.name = input("What's your name? ")
+    """someone who plays the game"""
+
+    def __init__(self):
+        self.exp = 1
+
+
+# self.name = input("What's your name? ")
 
 bob = Player()
 
@@ -24,85 +28,82 @@ introduction_text += " 0 to 9. One of the bulbs lights up and then "
 
 introduction_text += "another."
 
+
 def play_game(Player):
-	"""main game loop"""
+    """main game loop"""
 
-	print(introduction_text)
+    print(introduction_text)
 
-	while game_on:
+    while game_on:
 
-		questions = 3
-	
-		if Player.exp < 11:
-			mission(9, Player, questions)
-		elif Player.exp < 100:
-			mission(40, Player, questions)
-		else:
-			mission(99, Player, questions)
-	
-	
+        questions = 3
+
+        if Player.exp < 11:
+            mission(9, Player, questions)
+        elif Player.exp < 100:
+            mission(40, Player, questions)
+        else:
+            mission(99, Player, questions)
+
+
 def mission(level, Player, questions):
-		
-	print("\n\nIt's an adding game")
-	
-	score = 0
-	
-	for i in range(0, questions):
-	
-		
-		a = randint(1, level)
-		b = randint(1, level)
+    print("\n\nIt's an adding game")
 
-		exp_gain=0
-		#exp_bang is how excited to get about the exp_gain
-		exp_bang=""
+    score = 0
 
-		print(str(a) + " + " + str(b))
-	
-		ans = input("> ")
-	
-		#cast ans to int for comparison with the correct answer
-		#assuming it's a number as string...
-		ans = int(ans)	
-		
-		if ans == "quit":
-			#quit mission and stop playing
-			game_on = False
-			return 0
-		
 
-		
-		elif ans == a+b:
-			#correct
-			print("Correct")
-			score += 1
-		else:
-			#wrong
-			print("WRONGO")
-		
-	print("You scored " + str(score))	
+    exp_gain = 0
+    #exp_bang is how excited to get about the exp_gain
+    exp_bang = ""
+
+    for i in range(0, questions):
+
+        a = randint(1, level)
+        b = randint(1, level)
+
+        print(str(a) + " + " + str(b))
+
+        ans = input("> ")
+
+        # cast ans to int for comparison with the correct answer
+        # assuming it's a number as string...
+        ans = int(ans)
+
+        if ans == "quit":
+            # quit mission and stop playing
+            game_on = False
+            return 0
 
 
 
-	if score == questions:
-		#full score, bonus marks
-		exp_gain = int(level * (1 + randint(1,5)/20))
-		exp_bang="!!"
+        elif ans == a + b:
+            # correct
+            print("Correct")
+            score += 1
+        else:
+            # wrong
+            print("WRONGO")
 
-	elif score >= int(questions * .9):
-		#90% pass mark
-		exp_gain = int(level * (randint(5,11)/10))
-		exp_bang="!"
+    print("You scored " + str(score))
 
-	elif score >= int(questions * .5):
-		#50% not completely terrible, but NO BANG FOR YOU
-		exp_gain = int(level * (randint(1,3)/10))
-		exp_bang=""
+    if score == questions:
+        # full score, bonus marks
+        exp_gain = int(level * (1 + randint(1, 5) / 20))
+        exp_bang = "!!"
 
-	Player.exp += exp_gain
+    elif score >= int(questions * .9):
+        # 90% pass mark
+        exp_gain = int(level * (randint(5, 11) / 10))
+        exp_bang = "!"
 
-	print("+" + str(exp_gain) + " exp" + exp_bang)
-	print("Your exp: " + str(Player.exp))
-	
-	
+    elif score >= int(questions * .5):
+        # 50% not completely terrible, but NO BANG FOR YOU
+        exp_gain = int(level * (randint(1, 3) / 10))
+        exp_bang = ""
+
+    Player.exp += exp_gain
+
+    print("+" + str(exp_gain) + " exp" + exp_bang)
+    print("Your exp: " + str(Player.exp))
+
 play_game(bob)
